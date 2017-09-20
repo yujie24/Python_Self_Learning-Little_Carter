875d0604337aeda482d7cf30f13a57d3a415d09b
James Yu <yujieshhai@163.com>
2017-09-21 00:02:29 +0800

    comment line
    change the comment line

--- 第一章/1-2.py
+++ 第一章/1-2.py
@@ -1,9 +1,9 @@
-"""
-编写一个简短的小程序，打印 3 行：你的名字、出生日期，还有你最喜欢的颜色。打印结果应该类似这样：
-My name is Warren Sande.
-I was born January 1, 1970.
-My favorite color is blue.
-"""
+#编写一个简短的小程序，打印 3 行：你的名字、出生日期，还有你最喜欢的颜色。
+#打印结果应该类似这样：
+#My name is Warren Sande.
+#I was born January 1, 1970.
+#My favorite color is blue.
+
 
 print("My name is James Yu.")
 print("I was born August 15,1992")
