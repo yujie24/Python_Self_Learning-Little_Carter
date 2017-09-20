d54063ecf3ed4a6cddfa05e20f8172af9f6c4986
James Yu <yujieshhai@163.com>
2017-09-21 00:11:34 +0800

    chapter2

---
+++ Chapter2_Memory and Variables/2-4_calculate the minutes of a week.py
@@ -0,0 +1,7 @@
+#2-4 像上一章一样，在交互模式中让 Python 计算一周有多少分钟。不过，这一次要使用变量。
+#以 DaysPerWeek（每周天数）、HoursPerDay（每天小时数）和 MinutesPerHour（每小时分钟数）为名分别创建变量（或者也可以用自己取的变量名），然后将它们相乘。
+
+DaysPerWeek = 7
+HoursPerDay = 24
+MinutePerHour = 60
+print(DaysPerWeek*HoursPerDay*MinutePerHour)
