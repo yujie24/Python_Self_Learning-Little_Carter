aa50a496413df77609f929ef193a7ac04db6fee7
James Yu <yujieshhai@163.com>
2017-09-21 00:39:27 +0800

    chapter3

---
+++ Chapter3_Basic Mathematical Operation/3-1b_Area of a room.py
@@ -0,0 +1,10 @@
+#   3-1. 使用交互模式或者编写一个小程序解决下面的问题。
+
+#   (b) 计算一个 12.5m×16.7m 的矩形房间的面积和周长。
+
+length = 12.5
+width = 16.7
+area = length * width
+perimeter = (length + width) * 2
+print(area)
+print(perimeter)
